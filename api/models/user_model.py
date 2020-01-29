bd5966c38ee13b538288f70d32dec05642f5322b
ywalakamar <barxwells@gmail.com>
2020-01-29 22:52:31 +0300

    feature(register):user registration - create user registration endpoint - make corrections in user model

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -25,20 +25,20 @@ class User():
     def create_user(self):
         query = """
                     INSERT INTO users (first_name, last_name, email, tel, password, dl_path, car_reg)
-                    VALUES ('{0}', '{1}', '{2}', '{3}', '{4}','{5}','{}','{6}')
+                    VALUES ('{0}', '{1}', '{2}', '{3}', '{4}','{5}','{6}')
                 """.format(self.first_name, self.last_name, self.email, self.tel, self.password, self.dl_path, self.car_reg)
         CRUD.commit(query)
 
     @staticmethod
     def find_by_email(email):
-        query = "SELECT id, email, tel, name, password FROM users WHERE email = '{0}'".format(
+        query = "SELECT id, first_name, last_name, email, tel, password FROM users WHERE email = '{0}'".format(
             email)
         user = CRUD.readOne(query)
         return user
 
     @staticmethod
     def is_driver(user_id):
-        query = "SELECT name, dl_path, car_reg FROM users WHERE id = {0}".format(
+        query = "SELECT first_name, last_name, dl_path, car_reg FROM users WHERE id = {0}".format(
             user_id)
 
         driver = CRUD.readOne(query)
