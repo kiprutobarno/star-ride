c311a5c84e5f03a773d81b76a6abb585fffe98f6
ywalakamar <barxwells@gmail.com>
2020-01-30 12:08:14 +0300

    chore(authentication):JWT - use JWT authentication to protect endpoints

--- api/run.py
+++ api/run.py
@@ -1,14 +1,30 @@
 import os
 from flask import Flask, Blueprint
 from flask_restplus import Api, fields
+from flask_jwt_extended import JWTManager
 from .schema import create_tables
 
 from .views import auth
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
+jwt = JWTManager(app)
 
-api = Api(app, description='Star Ride API endpoint')
+app.config['JWT_SECRET_KEY'] = "superpower"
+
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
+
+api = Api(app,
+          authorizations=authorizations,
+          security='apiKey',
+          description='Star Ride API endpoint'
+          )
 
 create_tables()
 
--- api/views/auth.py
+++ api/views/auth.py
@@ -1,5 +1,7 @@
 from flask import Flask, abort, json, request
 from flask_restplus import Resource, fields, Model, Api
+from flask_jwt_extended import (
+    create_access_token, create_refresh_token, jwt_required, get_raw_jwt)
 from ..models import user_model
 from .helpers import validate_json, validate_email, check_for_blanks, check_data_type, validate_required, required_input
 app = Flask(__name__)
@@ -38,14 +40,6 @@ class Register(Resource):
             abort(409, 'An account with email {} already exists'.format(new_user.email))
         else:
             new_user.create_user()
-            # user = user_model.User.find_by_email(email)
-            # payload = {
-            #     "id": user['id'],
-            #     "email": user['email'],
-            #     "tel": user['tel'],
-            #     "first_name": user['first_name'],
-            #     "last_name": user['last_name']
-            # }
             return {
                 "status": "success",
                 "message": "{} registered".format(data['email'])
@@ -75,20 +69,19 @@ class Login(Resource):
         username = user_exists['first_name']
 
         if user_model.User.verify_password(password, password_hash):
-            # payload = {
-            #     "id": user_exists['id'],
-            #     "email": user_exists['email'],
-            #     "tel": user_exists['tel'],
-            #     "first_name": user_exists['first_name'],
-            #     "last_name": user_exists['last_name']
-            # }
+            payload = {
+                "id": user_exists['id'],
+                "email": user_exists['email']
+            }
+            access_token = create_access_token(identity=payload)
             return {
                 "status": "success",
-                "message": "{} logged in".format(username)
+                "message": "{} logged in".format(username),
+                "token": access_token
             }, 200
 
         else:
             return {
                 "status": "fail",
-                "message": "wrong password"
+                "message": "wrong password",
             }, 400
